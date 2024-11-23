d4af5c56b2d9a5abff79d4cb829e86ba0465616e
SamalaSumanth0262 <chintusamala96@gmail.com>
2024-11-23 21:02:57 +0000

    conversion serive without the rate service implmentation

--- conversion-gateway/src/main/java/api/conversion_gateway/ConversionGatewayApplication.java
+++ conversion-gateway/src/main/java/api/conversion_gateway/ConversionGatewayApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConversionGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConversionGatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ConversionGatewayApplication.class, args);
+  }
 }
