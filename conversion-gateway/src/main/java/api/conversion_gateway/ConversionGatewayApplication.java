674593cbafc3b026a44d00b78ec36e4c4435523b
SamalaSumanth0262 <chintusamala96@gmail.com>
2024-11-23 17:56:01 +0000

    conversion api gateway init

---
+++ conversion-gateway/src/main/java/api/conversion_gateway/ConversionGatewayApplication.java
@@ -0,0 +1,13 @@
+package api.conversion_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionGatewayApplication.class, args);
+	}
+
+}
---
+++ conversion-gateway/src/test/java/api/conversion_gateway/ConversionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package api.conversion_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
